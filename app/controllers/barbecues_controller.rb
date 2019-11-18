7ed9b9f38490aab4ea944e2134a3d9385c213461
pierrelouis geoffroy <pierrelouis.geoffroy@gmail.com>
2019-11-18 18:52:35 +0100

    super

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -1,14 +1,23 @@
 class BarbecuesController < ApplicationController
   def index
-  end
-
-  def show
+    @barbecues = Barbecue.all
   end
 
   def new
+    @barbecue = Barbecue.new
   end
 
   def create
+    @barbecue = Barbecue.new(barbecue_params)
+    if @barbecue.save
+      redirect_to barbecue_path(@barbecue)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @barbecue = Barbecue.find(params[:id])
   end
 
   def edit
@@ -18,5 +27,14 @@ class BarbecuesController < ApplicationController
   end
 
   def destroy
+    @barbecue = Barbecue.find(params[:id])
+    @barbecue.destroy
+    redirect_to barbecues_path
+  end
+
+  private
+
+  def barbecue_params
+    params.require(:barbecue).permit(:name)
   end
 end
