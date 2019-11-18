41c3cfcda55c141fb5f0fd130131a5686b41a883
mickael Verdin <mickael.verdin@numericable.fr>
2019-11-18 23:26:59 +0100

    add db seed users & rentals + method random id(users barbecue + rental)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,69 @@
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def id_users_random_barbecue
+  (1..20).to_a.sample(10)
+end
 
+def id_users_random_rental
+  array_id_selected = (1..20).to_a.sample
+  users_id_barbecue.each do |num|
+    array_id_selected.delete_if { |id_delete| id_delete == num }
+  end
+  return array_id_selected
+end
+
+users_id_barbecue = id_users_random_barbecue
+users_id_rental = id_users_random_rental
+barbecues_id_rental = id_barbecue_rental
+count = 0
+
+puts "---- Delete all items in database ----"
 Barbecue.destroy_all
 Rental.destroy_all
 User.destroy_all
 
-user_attributes = [ {
-                      email: "#{Faker::Name.last_name} #{['@free.fr',
-                                                          '@numericable.fr',
-                                                          '@aol.fr','@orange.fr'].sample}"
+def createSeedUsers
+puts "---- Seed model User (20 instances) ----"
+  users_attributes = []
+  prefixes_mail = ['@free.fr', '@numericable.fr', '@aol.fr','@orange.fr']
+  10.times  { users << {  email: "#{Faker::Name.last_name}.#{Faker::Name.first_name}#{prefixes_mail.sample}",
+                          encrypted_password: "a652a3922a113s8" } }
+  User.create!(users_attributes)
+end
 
+def createSeedBarbecues
+  puts "---- Seed model Barbecue (10 instances) ----"
+  barbecues_attributes = []
+  10.times  { count += 1
+              barbecues <<
+              {
+                name:             Faker::Name.name,
+                description:      Faker::Lorem.paragraph,
+                category:         ['charbon', 'gaz', 'électrique'].sample,
+                price:            ((100...600).to_a).sample,
+                location:         Faker::Address.city,
+                user_id:          users_id_barbecue[count]
+              }
+            }
+  Barbecue.create!(barbecues_attributes)
+end
 
+def createSeedrentals
+  puts "---- Seed model Rentals (10 instances) ----"
+  rentals_attributes = []
+  10.times  { count += 1
+              rentals <<
+              {
+                start_date:       Time.now,
+                end_date:         Faker::Date.between(from: 150.days.ago, to: Date.today),
+                barbecue_id:      count,
+                user_id:          users_id_rental[count]
+              }            
+            }
+  Rental.create!(rentals_attributes)
+end
 
+createSeedUsers
+createSeedBarbecues
+createSeedrentals
 
-
-
-
-
-
-
-
-}]
-
-
-
-
-
-
-
-
-barbecue_attributes = [ {
-                          user:             u1,
-                          name:             Faker::Name.name,
-                          description:      Faker::Lorem.paragraph,
-                          category:         ['charbon', 'gaz', 'électrique'].sample,
-                          price:            ((100...600).to_a).sample,
-                          location:         Faker::Address.city
-                        }
-]
-
-Barbecue.create!(barbecue_attributes)
+puts "---- Seed 'ok' ----"
