752de53d0c51a49ef53da4a8ae49d1031e903dbc
Mickael Verdin <mickael.verdin@numericable.fr>
2019-11-18 19:29:22 +0100

    db seed barbecue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Barbecue.destroy_all
+Rental.destroy_all
+User.destroy_all
+
+user_attributes = [ {
+                      email: "#{Faker::Name.last_name} #{['@free.fr',
+                                                          '@numericable.fr',
+                                                          '@aol.fr','@orange.fr'].sample}"
+
+
+
+
+
+
+
+
+
+
+
+}]
+
+
+
+
+
+
+
+
+barbecue_attributes = [ {
+                          user:             u1,
+                          name:             Faker::Name.name,
+                          description:      Faker::Lorem.paragraph,
+                          category:         ['charbon', 'gaz', 'électrique'].sample,
+                          price:            ((100...600).to_a).sample,
+                          location:         Faker::Address.city
+                        }
+]
+
+Barbecue.create!(barbecue_attributes)
