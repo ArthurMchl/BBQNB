156bf5208123b49f32d8306ae4841aff821f8d41
mickael Verdin <mickael.verdin@numericable.fr>
2019-11-19 06:25:16 +0100

    add variables + parameters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,32 @@
+users_id_rental = []
+users_id_barbecue = []
+users = []
+barbecues = []
+rentals = []
+
+count = 0
 
 def id_users_random_barbecue
   (1..20).to_a.sample(10)
 end
 
-def id_users_random_rental
-  array_id_selected = (1..20).to_a.sample
-  users_id_barbecue.each do |num|
+def id_users_random_rental(array_users_id_barbecue)
+  array_id_selected = (1..20).to_a
+  array_users_id_barbecue.each do |num|
     array_id_selected.delete_if { |id_delete| id_delete == num }
   end
-  return array_id_selected
 end
 
 users_id_barbecue = id_users_random_barbecue
-users_id_rental = id_users_random_rental
-barbecues_id_rental = id_barbecue_rental
-count = 0
+users_id_rental = id_users_random_rental(users_id_barbecue)
+
 
 puts "---- Delete all items in database ----"
 Barbecue.destroy_all
 Rental.destroy_all
 User.destroy_all
 
-def createSeedUsers
+def createSeedUsers(users)
 puts "---- Seed model User (20 instances) ----"
   users_attributes = []
   prefixes_mail = ['@free.fr', '@numericable.fr', '@aol.fr','@orange.fr']
@@ -30,10 +35,10 @@ puts "---- Seed model User (20 instances) ----"
   User.create!(users_attributes)
 end
 
-def createSeedBarbecues
+def createSeedBarbecues (users_id_barbecue, barbecues)
   puts "---- Seed model Barbecue (10 instances) ----"
   barbecues_attributes = []
-  10.times  { count += 1
+  10.times  { count =+ 1
               barbecues <<
               {
                 name:             Faker::Name.name,
@@ -45,12 +50,13 @@ def createSeedBarbecues
               }
             }
   Barbecue.create!(barbecues_attributes)
+  count = 0
 end
 
-def createSeedrentals
+def createSeedrentals(users_id_rental, rentals)
   puts "---- Seed model Rentals (10 instances) ----"
   rentals_attributes = []
-  10.times  { count += 1
+  10.times  { count =+ 1
               rentals <<
               {
                 start_date:       Time.now,
@@ -60,10 +66,11 @@ def createSeedrentals
               }            
             }
   Rental.create!(rentals_attributes)
+count = 0
 end
 
-createSeedUsers
-createSeedBarbecues
-createSeedrentals
+createSeedUsers(users)
+createSeedBarbecues(users_id_barbecue, barbecues)
+createSeedrentals(users_id_rental,rentals )
 
 puts "---- Seed 'ok' ----"
