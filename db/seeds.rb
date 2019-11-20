74453be88bf73014cf73c85fa307c972fd79479b
pierrelouis geoffroy <pierrelouis.geoffroy@gmail.com>
2019-11-20 15:02:08 +0100

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,44 @@ puts '---- Seed model User (4 instances) ----'
 
 
 micka = User.create(
- {
   email: 'mika@gmail.com',
   password: 'azerty'
- }
- )
+)
 
 
  pierre = User.create(
- {
    email: 'pierre@gmail.com',
   password: 'azerty'
- }
- )
+)
 
  arthur = User.create(
- {
   email: 'arthur@gmail.com',
   password: 'azerty'
- }
- )
+)
 
 werner= User.create(
-  {
   email: 'werner@gmail.com',
   password: 'azerty'
-  }
- )
+)
 
 puts '---- Seed model User "ok" ---'
 
 puts '---- Seed model Barbecue (8 instances) ----'
 
 barbecue_mick_un = Barbecue.create(
-  {
-    name:         'Barbecue super 3000++',
-    description:  "Ce pack est composé d'un brasero sur trois pieds en acier ø 59 cm, d'une grille de cuisson de ø 60 cm (idéale pour vos
-                  cuissons de poissons, de légumes) et d'un kit marshmallow qui fera le bonheur des plus jeunes, à condition qu'un adulte soit présent !
-                  Attention : Le brasero ne doit pas être posé sur une terrasse en bois. La chaleur qui est générée peut abîmer le sol.",
-    category:     'gaz',
-    price:        330,
-    location:     'Lille',
-    user:         micka,
-  }
+  name:         'Barbecue super 3000++',
+  description:  "Ce pack est composé d'un brasero sur trois pieds en acier ø 59 cm, d'une grille de cuisson de ø 60 cm (idéale pour vos
+                cuissons de poissons, de légumes) et d'un kit marshmallow qui fera le bonheur des plus jeunes, à condition qu'un adulte soit présent !
+                Attention : Le brasero ne doit pas être posé sur une terrasse en bois. La chaleur qui est générée peut abîmer le sol.",
+  category:     'gaz',
+  price:        330,
+  location:     'Lille',
+  user:         micka,
 )
+barbecue_mick_un.remote_photo_url = "https://www.google.fr/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwir74ih9vjlAhVL8uAKHdo4BUsQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.manomano.fr%2Fp%2Fbarbecue-charbon-moorea-grille-diametre-54-cm-148783&psig=AOvVaw1mcjXhNmTpv8cORNV2_UcA&ust=1574343903085213"
+barbecue_mick_un.save
 
 barbecue_mick_deux = Barbecue.create(
-  {
     name:         'Weber',
     description:  "Stand 4 pieds avec système de démontage facile, idéal pour les espaces types balcon,
                   terrasse
@@ -64,11 +55,9 @@ barbecue_mick_deux = Barbecue.create(
     price:        500,
     location:     'Lens',
     user:         micka,
-  }
 )
 
 barbecue_pierre_un = Barbecue.create(
-  {
     name:         'Esprit Barbecue',
     description:  "La locomotive Joe's Wild West est équipé de deux chambres qui permettent de cuir ou
                   fumer vos aliments à des températures comprises entre 90 et 130°C ! Une fabrication à la
@@ -78,11 +67,9 @@ barbecue_pierre_un = Barbecue.create(
     price:        100,
     location:     'Lille',
     user:         pierre
-  }
 )
 
 barbecue_pierre_deux = Barbecue.create(
-  {
     name:         'Barbecook',
     description:  "Le Royal 320 noir est un barbecue à gaz qui vous offre toute une multitude de
                   possibilités en matière de cuisson, mais également un confort important pendant la
@@ -92,11 +79,9 @@ barbecue_pierre_deux = Barbecue.create(
     price:        100,
     location:     'Lille',
     user:         pierre
-  }
 )
 
 barbecue_arthur_un = Barbecue.create(
-  {
     name:         'Somatic barbecues',
     description:  "Barbecue à gaz Broil King Impérial XL, pour 18 à 20 personnes, grilles de cuisson en
                   fonte et inox, 6 brûleurs, un brûleur arrière pour la rôtissoire et un réchaud latéral.
@@ -105,11 +90,9 @@ barbecue_arthur_un = Barbecue.create(
     price:        100,
     location:     'Lille',
     user:         arthur
-  }
 )
 
 barbecue_arthur_deux = Barbecue.create(
-  {
     name:         'Dixneuf',
     description:  "La locomotive Joe's Wild West est équipé de deux chambres qui permettent de cuir ou
                   fumer vos aliments à des températures comprises entre 90 et 130°C ! Une fabrication à la
@@ -119,11 +102,9 @@ barbecue_arthur_deux = Barbecue.create(
     price:        270,
     location:     'Seclin',
     user:         arthur
-  }
 )
 
 barbecue_werner_un = Barbecue.create(
-  {
     name:         'Grill`up',
     description:  "Le barbecue à charbon de bois Weber conservera toutes les saveurs de vos aliments
                   grâce à son système de cuisson à chaleur tournante. Vous pourrez cuisiner avec le couvercle
@@ -133,11 +114,9 @@ barbecue_werner_un = Barbecue.create(
     price:        790,
     location:     'Lille',
     user:         werner
-  }
 )
 
 barbecue_werner_deux = Barbecue.create(
-  {
     name:         'Grill O`Bois',
     description:  "Le barbecue à charbon de bois Weber conservera toutes les saveurs de vos aliments
                   grâce à son système de cuisson à chaleur tournante. Vous pourrez cuisiner avec le couvercle
@@ -147,7 +126,6 @@ barbecue_werner_deux = Barbecue.create(
     price:        1200,
     location:     'Lille',
     user:         werner
-  }
 )
 
 puts '---- Seed model Barbecue "ok" ---'
@@ -155,75 +133,59 @@ puts '---- Seed model Barbecue "ok" ---'
 puts '---- Seed model Rental (8 instances) ----'
 
 rental_mv_pr1 = Rental.create(
-  {
     start_date:       Time.new(2017, 01, 31, 2, 2, 2, "+02:00"),
     end_date:         Time.new(2019, 12, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_mick_un.id,
     user_id:          pierre.id
-  }
 )
 
 rental_mv_pr2 = Rental.create(
-  {
     start_date:       Time.now,
     end_date:         Time.new(2020, 01, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_pierre_un.id,
     user_id:          micka.id
-  }
 )
 
 rental_art_wnr1 = Rental.create(
-  {
     start_date:       Time.now,
     end_date:         Time.new(2020, 12, 02, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_arthur_un.id,
     user_id:          werner.id
-  }
 )
 
 rental_art_wnr2 = Rental.create(
-  {
     start_date:       Time.now,
     end_date:         Time.new(2020, 05, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_werner_un.id,
     user_id:          arthur.id
-  }
 )
 
 rental_mv_art1 = Rental.create(
-  {
     start_date:       Time.new(2019, 01, 31, 2, 2, 2, "+02:00"),
     end_date:         Time.new(2019, 12, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_mick_deux.id,
     user_id:          arthur.id
-  }
 )
 
 rental_mv_art2 = Rental.create(
-  {
     start_date:       Time.now,
     end_date:         Time.new(2019, 12, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_arthur_deux.id,
     user_id:          micka.id
-  }
 )
 
 rental_wnr_pr1 = Rental.create(
-  {
     start_date:       Time.now,
     end_date:         Time.new(2020, 01, 31, 2, 2, 2, "+02:00"),
     barbecue_id:      barbecue_pierre_deux.id,
     user_id:          werner.id
-  }
 )
 
 rental_wnr_pr2 = Rental.create(
-  {
-    start_date:       Time.new(2020, 01, 31, 2, 2, 2, "+02:00"),
-    end_date:         Time.new(2021, 01, 31, 2, 2, 2, "+02:00"),
-    barbecue_id:      barbecue_werner_deux.id,
-    user_id:          pierre.id
-  }
+  start_date:       Time.new(2020, 01, 31, 2, 2, 2, "+02:00"),
+  end_date:         Time.new(2021, 01, 31, 2, 2, 2, "+02:00"),
+  barbecue_id:      barbecue_werner_deux.id,
+  user_id:          pierre.id
 )
 
 puts '---- Seed model Rental "ok" ---'
