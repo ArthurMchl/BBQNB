61ad8758fa7e08f0eee8fad3d0c0c21ab058bd12
Werner Soudain <werner.sdn@gmail.com>
2019-11-22 14:30:08 +0100

    dynamic price

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,25 @@
 import "bootstrap";
 
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-flatpickr(".datepicker", {})
-
+// mapbox import
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-initMapbox();
 
+// flatpickr imports
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+// customs components
+import { dynamicPrice } from '../components/dynamic_price';
+
+
+flatpickr(".datepicker", {})
+
+initMapbox();
+
+dynamicPrice();
+
 
 
  const hashRental = document.getElementById('rental-form');
